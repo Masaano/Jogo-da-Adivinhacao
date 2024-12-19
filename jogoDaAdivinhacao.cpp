dccec0cf46795c98046235d002621e01b3d36871
Mateus Martins <mateusmartins27000@gmail.com>
2024-12-19 11:50:46 -0300

    Impressao de texto e int do jogo da adivinhaçao.

---
+++ jogoDaAdivinhacao.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "**************************************" << endl;
+    cout << "* Bem-vindos ao jogo da adivinhacao! *" << endl;
+    cout << "**************************************" << endl;
+
+    int nSecreto = 85;
+
+    cout << "O numero secreto e " << nSecreto << ". Nao conte para ninguem!" << endl;
+}
